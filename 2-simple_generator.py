83f149bcf13a38940088144d07412276a67da539
saidi <saidi.ahamada@gmail.com>
2022-05-12 23:07:02 +0200

    add some comments

--- 2-simple_generator.py
+++ 2-simple_generator.py
@@ -1,10 +1,30 @@
+# 1 - Create generator with yield statement
+print("1 - Create generator with yield statement")
 def simple_generator_func():
     yield 1
     yield 2
     yield 3
-     
+
+print(type(simple_generator_func()))
+data_generator = simple_generator_func()
+print(next(data_generator))
+print(next(data_generator))
+print(next(data_generator))
+
+
+# 2 - Create generator with parenthesis     
+print("2 - Create generator with parenthesis")
 def simple_generator_expr():
     return (i for i in range(10))
+
+print(type(simple_generator_expr()))
+data_generator = (simple_generator_expr())
+print(next(data_generator))
+print(next(data_generator))
+
+
+# 3 - Create a generator which handle odd numbers
+print("3 - Create a generator which handle odd numbers")
 
 def odd_generator(max=10):
     number = 1
@@ -27,17 +47,3 @@ print(next(odd_numbers))
 print(next(odd_numbers))
 print(next(odd_numbers))
 print(next(odd_numbers))
-
-
-# print(type(simple_generator()))
-
-# data = (simple_generator())
-# print(next(data))
-# print(next(data))
-
-
-# data_2 = simple_generator_bracket()
-# print(type(data_2))
-# print(next(data_2))
-# print(next(data_2))
-# print(next(data_2))
